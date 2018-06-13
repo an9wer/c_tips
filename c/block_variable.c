4dcde272a35012726797f8d8b1c17095aa3b560d
an9wer <an9wer@gmail.com>
2018-06-13 20:52:39 +0800

    Restucture

--- c/block_variable.c
+++ c/block_variable.c
@@ -7,8 +7,9 @@
 
 int main(void)
 {
+    { int j = 1; };
     for (int i = 1; i < 10; i++);
-    printf("we cannot get vaiable 'i' here\n");
+    printf("we cannot get vaiable 'j' and 'i' here\n");
 
     return 0;
 }
---
+++ nodejs/block_variable.js
@@ -0,0 +1,13 @@
+/*
+ * JavaScript has no concept of block-level scoping, meaning variables defined
+ * inside of block statements are actually created in the containing function,
+ * not within the statement.
+ */
+
+function main() {
+    { var j = 1; }
+    for (var i = 1; i < 10; i++);
+    console.log("we can get variable 'i' and 'j' here, which are " + i + " and " + j + ".");
+}
+
+main()
