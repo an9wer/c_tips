022a9a2a01594a880a05d77a8448688b957a6b59
an9wer <an9wer@gmail.com>
2018-10-03 22:16:59 +0800

    Bash: pitfulls about 'set -e'

--- Bash/set_options.sh
+++ Bash/set_options.sh
@@ -4,4 +4,5 @@ set -h  # or 'set -o hashall'
 
 # Abort script at first error, when a command exits with non-zero status
 # (except in until or while loops, if-tests, list constructs)
+# Note: set -e also has some problems (thx: https://mywiki.wooledge.org/BashFAQ/105)
 set -e # or 'set -o errexit'
